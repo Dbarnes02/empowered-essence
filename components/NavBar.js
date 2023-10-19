39e80f2ed491c0c6edc7d28b690e9eecf08a57ff
Diana Barnes <90351979+Dbarnes02@users.noreply.github.com>
2023-10-19 00:30:25 -0600

    Creation of empowered essence app

---
+++ components/NavBar.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function NavBar() {
+    return (
+        <Image
+        scr="images/ee-logo.webp"
+        width={250}
+        height={250}
+        alt="Empowered Essence Logo"
+        />
+    )
+}
